68a5e44b2bd3797547484c782bd593e08289df2c
Mario Hernandez <mariohdz01@hotmail.com>
2021-03-09 14:57:26 -0600

    first commit

---
+++ src/main/java/com/tts/WebTest/Service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.tts.WebTest.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+	public String greet() {
+		return "Hello, World";
+	}
+}
